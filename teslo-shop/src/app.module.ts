b3bbc0e7f6fddea9765ab7e66aa98be6c688d1cb
rene <diaz.de.sandi.7@gmail.com>
2025-12-16 21:53:54 -0800

    fix(app): enforce SSL configuration for TypeORM connection

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -17,7 +17,7 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRoot({
-      ssl: process.env.STAGE === 'prod',
+      ssl: { rejectUnauthorized: false }, // Always on for DO
       extra: {
         ssl: process.env.STAGE === 'prod'
               ? { rejectUnauthorized: false }
